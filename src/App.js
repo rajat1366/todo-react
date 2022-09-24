eee3ca42bbdf3e2bdc2bee41a316398878963418
Rajat Kumar <rajat1366@gmail.com>
2022-09-24 11:14:37 +0530

    Styled Components

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
-import './App.css';
+import styled from 'styled-components';
 import TodoList from './todos/TodoList';
 
-function App() {
-  return (
-    <div className="App">
+const AppContainer = styled.div`
+    margin: 1rem;
+    font-family: Arial, Helvetica, sans-serif;
+    color: #222222;
+    width: 100vw;
+    height: 100vh;
+`;
+
+const App = () => (
+    <AppContainer>
         <TodoList />
-    </div>
-  );
-}
+    </AppContainer>
+);
 
 export default App;
--- src/todos/TodoList.js
+++ src/todos/TodoList.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
+import styled from 'styled-components';
 import NewTodoForm from './NewTodoForm';
 import TodoListItem from './TodoListItem';
 import {
@@ -8,7 +9,11 @@ import {
     getIncompleteTodos,
 } from './selectors';
 import { loadTodos, removeTodoRequest, markTodoAsCompletedRequest } from './thunks';
-import './TodoList.css';
+
+const ListWrapper = styled.div`
+    max-width: 700px;
+    margin: auto;
+`;
 
 const TodoList = ({ completedTodos, incompleteTodos, onRemovePressed, onCompletedPressed, isLoading, startLoadingTodos }) => {
     useEffect(() => {
@@ -17,7 +22,7 @@ const TodoList = ({ completedTodos, incompleteTodos, onRemovePressed, onComplete
     
     const loadingMessage = <div>Loading todos...</div>;
     const content = (
-        <div className="list-wrapper">
+        <ListWrapper>
             <NewTodoForm />
             <h3>Incomplete:</h3>
             {incompleteTodos.map(todo => <TodoListItem
@@ -29,7 +34,7 @@ const TodoList = ({ completedTodos, incompleteTodos, onRemovePressed, onComplete
                 todo={todo}
                 onRemovePressed={onRemovePressed}
                 onCompletedPressed={onCompletedPressed}/>)}
-        </div>
+        </ListWrapper>
     );
     return isLoading ? loadingMessage : content;
 };
